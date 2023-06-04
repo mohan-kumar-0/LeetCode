c1645154b81ccd997324e22bd0bf33b180e8629d
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-06-04 09:36:25 +0530

    Time: 12 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        int a[26] = {0};
+        for(int i=0;s[i];i++)
+            a[s[i]-'a']=1;
+        int ans = 0;
+        for(int i=0;i<26;i++)
+            ans+=a[i];
+        return ans;
+    }
+};
