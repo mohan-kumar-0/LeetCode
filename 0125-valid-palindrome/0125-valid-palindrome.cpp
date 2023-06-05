772a4e0cf6ddb093c46e660c1849c7698969800b
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-06-05 07:10:40 +0530

    Time: 9 ms (26.27%), Space: 8.1 MB (8.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -11,6 +11,8 @@ public:
         return res;
     }
     bool isPalindrome(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
         s = transform(s);
         int i=0,j=s.size()-1;
         while(i<j)
