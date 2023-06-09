a0e6f401e9dac42596355a47a30e619e925e5edd
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-06-09 21:00:22 +0530

    Time: 0 ms (100.00%), Space: 9 MB (23.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        ios_base::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
         int ctr=0;
         int n = nums.size();
         for(int i=0;i<n;++i)
