66e3a2163858d725c00ac860350d9c23495c808b
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-06-09 21:04:34 +0530

    Time: 17 ms (30.26%), Space: 18.4 MB (34.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,6 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL); cout.tie(NULL);
+        if(nums.size()==0)
+            return 1;
         int i=0,j=0;
         int n = nums.size();
         while(j<n){
