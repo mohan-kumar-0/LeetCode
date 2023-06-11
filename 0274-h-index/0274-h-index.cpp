7790a9ad76b8b9cf901677adff178f17c7e2920d
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-06-11 07:38:49 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (27.58%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,7 +3,7 @@ public:
     int hIndex(vector<int>& citations) {
         int i=0;
         sort(citations.begin(),citations.end(),greater<int>());
-        for(i=0;i<citations.size();++i){
+        for(;i<citations.size();++i){
             if(citations[i]<=i){
                 return i;
             }
