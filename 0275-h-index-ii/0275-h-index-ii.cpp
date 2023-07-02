5fd581463fcbdd1fa713034a92369660ac949d30
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-07-02 21:49:11 +0530

    Time: 19 ms (71.34%), Space: 18.6 MB (23.52%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int h = 0;
+        for(int i=0;i<n;++i){
+            h = n - i;
+            if(citations[i]>=h)
+                return h;
+        }
+        return 0;
+    }
+};
