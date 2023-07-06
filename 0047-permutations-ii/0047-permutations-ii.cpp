b96074ed68688de269cdc4d951343f3a53f51ff3
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-07-06 19:32:23 +0530

    Time: 4 ms (92.54%), Space: 8.3 MB (94.14%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        ans.emplace_back(nums);
+        ans.push_back(nums);
         while(next_permutation(nums.begin(), nums.end())){
-            ans.emplace_back(nums);
+            ans.push_back(nums);
         }
         return ans;
     }
