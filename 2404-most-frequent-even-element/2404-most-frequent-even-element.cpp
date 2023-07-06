c080e2548c9a99dbb6c4cdce8345af4baa6dc13a
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-07-06 20:40:19 +0530

    Time: 49 ms (99.75%), Space: 30.9 MB (95.52%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
-        int k = -1;
-        int ctr = -1;
+        int a[100002] = {0};
+        int ctr = 0;
         int ans = -1;
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(auto x: nums)
-            cout<<x<<" "; cout<<"\n";
-        for(int i=0;i<n;){
-            ctr = 1;
-            for(int j=i+1;j<n && nums[j]==nums[i];++j)
-                ++ctr;
-            if(ctr>k && nums[i]%2==0){
-                k = ctr;
-                ans = nums[i];
-            }
-            i+=ctr;
+        int maxNum = -1;
+        for(int x: nums){
+            ++a[x];
+            maxNum = max(maxNum,x);
         }
+        for(int i=0;i<=maxNum;i+=2)
+            if(a[i]>ctr){
+                ctr = a[i];
+                ans = i;
+            }
         return ans;
     }
 };
