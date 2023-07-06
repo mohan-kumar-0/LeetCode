37e13318c40ad9be33caec37a1a676de65db925d
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-07-06 19:31:56 +0530

    Time: 6 ms (83.21%), Space: 8.3 MB (94.14%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
-        int fact = 1;
-        for(int i=2;i<=n;++i)
-            fact*=i;
-        set<vector<int>> permutations;
-        permutations.insert(nums);
-        for(int i=1;i<fact;++i){
-            next_permutation(nums.begin(),nums.end());
-            permutations.insert(nums);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        ans.emplace_back(nums);
+        while(next_permutation(nums.begin(), nums.end())){
+            ans.emplace_back(nums);
         }
-        vector<vector<int>> uniquePerms;
-        for(auto x: permutations)
-            uniquePerms.push_back(x);
-        return uniquePerms;
+        return ans;
     }
 };
