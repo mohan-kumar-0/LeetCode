fe969ee74f221f34ba8df066b4123d3ee7984a96
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-09-06 19:46:13 +0530

    Time: 131 ms (50.66%), Space: 108.8 MB (6.41%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        vector<int> contiguousZeroes;
+        int ctr = 0;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]==0){
+                ctr=0;
+                while(i<nums.size() && nums[i]==0){
+                    ++i; ++ctr;
+                }
+                contiguousZeroes.push_back(ctr);
+                ctr = 0;
+            }
+        }
+        if(ctr)
+            contiguousZeroes.push_back(ctr);
+        long long ans = 0;
+        for(long long x: contiguousZeroes)
+            ans += (x*(x+1))/2;
+        return ans;
+    }
+};
