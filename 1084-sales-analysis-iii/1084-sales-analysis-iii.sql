28360854d62357b8ee2d97238fd9c6a2b9e22801
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-09-12 20:23:19 +0530

    Time: 2192 ms (53.79%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,5 +1,6 @@
 select s.product_id, p.product_name
-from sales s, product p
+from sales s left join product p
+on s.product_id = p.product_id
 where s.product_id = p.product_id
 group by s.product_id, p.product_name
 having min(s.sale_date) >= '2019-01-01' 
