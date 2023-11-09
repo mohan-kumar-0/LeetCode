4f3769e87e656083748dd0b8ce165ef32c103ce2
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-11-09 19:15:25 +0530

    Time: 85 ms (19.09%), Space: 10.1 MB (44.49%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,12 +1,34 @@
 class Solution {
+    set<vector<int>> permutations;
+    vector<bool> visited;
+    vector<int> currPerm;
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(), nums.end())){
-            ans.push_back(nums);
+    
+    void dfs(vector<int>& nums, int n, int curr){
+        if(curr==n){
+            permutations.insert(currPerm);
         }
+        else{
+            for(int i=0;i<n;++i)
+                if(!visited[i]){
+                    visited[i] = true;
+                    currPerm.push_back(nums[i]);
+                    dfs(nums,n,curr+1);
+                    currPerm.pop_back();
+                    visited[i] = false;
+                }
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        permutations.clear();
+        for(int i=0;i<n;++i)
+            visited.push_back(false);
+        dfs(nums,n,0);
+        vector<vector<int>> ans;
+        for(vector<int> permutation: permutations)
+            ans.push_back(permutation);
         return ans;
     }
 };
