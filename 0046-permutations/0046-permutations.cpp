d97071c07492af530033cc9116ceeefdf9e70167
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2023-11-09 19:13:09 +0530

    Time: 5 ms (31.35%), Space: 8.5 MB (22.43%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,16 +1,31 @@
 class Solution {
+    vector<vector<int>> permutations;
+    vector<bool> visited;
+    vector<int> currPerm;
 public:
+    
+    void dfs(vector<int>& nums, int n, int curr){
+        if(curr==n){
+            permutations.push_back(currPerm);
+        }
+        else{
+            for(int i=0;i<n;++i)
+                if(!visited[i]){
+                    visited[i] = true;
+                    currPerm.push_back(nums[i]);
+                    dfs(nums,n,curr+1);
+                    currPerm.pop_back();
+                    visited[i] = false;
+                }
+        }
+    }
+    
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        int fact = 1;
-        for(int i=2;i<=n;++i)
-            fact*=i;
-        vector<vector<int>> permutations;
-        permutations.push_back(nums);
-        for(int i=1;i<fact;++i){
-            next_permutation(nums.begin(),nums.end());
-            permutations.push_back(nums);
-        }
+        permutations.clear();
+        for(int i=0;i<n;++i)
+            visited.push_back(false);
+        dfs(nums,n,0);
         return permutations;
     }
 };
