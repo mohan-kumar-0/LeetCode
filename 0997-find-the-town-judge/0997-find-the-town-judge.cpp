0ee093d327aa7c17f70313d4512335de27e053df
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-02-22 07:18:12 +0530

    Time: 94 ms (99.74%), Space: 65 MB (57.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
+    
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
+    }
+    
     int findJudge(int n, vector<vector<int>>& trust) {
         int trustCount[1001] = {0};
         int judge = -1;
-        for(vector<int> pair: trust){
-            ++trustCount[pair[1]];
-            --trustCount[pair[0]];
+        int size = trust.size();
+        for(int i=0;i<size;++i){
+            ++trustCount[trust[i][1]];
+            --trustCount[trust[i][0]];
         }
         // for(int i=1;i<=n;++i)
         //     cout<<trusts[i]<<"\t"; cout<<"\n";
