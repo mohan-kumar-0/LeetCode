b94426405d9cca6f8f5eb52b0cfb0a7a182a8c1f
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-03-06 09:37:37 +0530

    Time: 11 ms (43.85%), Space: 10.6 MB (53.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,6 +8,12 @@
  */
 class Solution {
 public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    
     bool hasCycle(ListNode *head) {
         
         ListNode *slowptr = head;
