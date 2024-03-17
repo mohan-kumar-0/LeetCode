77924eddb96790aff6f1e9dbdad4a2b7d9d90521
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-03-17 08:50:06 +0530

    Time: 378 ms (5.10%), Space: 20.5 MB (29.62%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,7 +5,6 @@ public:
         sort(intervals.begin(),intervals.end());
         for(int i=1;i<intervals.size();++i)
             if(intervals[i][0]<=intervals[i-1][1]){
-                //cout<<i<<" "<<intervals[i][0]<<" "<<intervals[i][1];
                 if(intervals[i][1]>intervals[i-1][1])
                     intervals[i-1][1] = intervals[i][1];
                 intervals.erase(intervals.begin()+i);
