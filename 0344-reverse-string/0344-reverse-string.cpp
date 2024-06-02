308890a7f14762eece2cad26b3b04efc3fd20408
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-06-02 16:28:03 +0530

    Time: 18 ms (53.67%), Space: 27.1 MB (7.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    
     void reverseString(vector<char>& s) {
         reverse(s.begin(),s.end());
     }
