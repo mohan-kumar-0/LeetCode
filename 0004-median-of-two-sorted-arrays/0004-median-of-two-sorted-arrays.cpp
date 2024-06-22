1d307ddd8d3d5d44ba205f574f87f94c7d6beda9
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-06-22 10:34:40 +0530

    Time: 11 ms (98.82%), Space: 94.3 MB (97.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        /*
-        Instead of making two cases for odd and even, add the same element twice if it's for an odd thing
-        m1 + m2 = (size1+size2)/2 coudl be used to update the m1 and m2 values
-        */
         int n = nums1.size();
         int m = nums2.size();
-        if (n > m)
-            return findMedianSortedArrays(nums2, nums1);
-
-        int l = 0;
-        int r = n;
-
-        while (l <= r) {
-            int mid1 = (l + r) / 2;
-            int mid2 = (n + m + 1) / 2 - mid1;
-            int l1 = mid1 == 0 ? INT_MIN : nums1[mid1-1];
-            int l2 = mid2 == 0 ? INT_MIN : nums2[mid2-1];
-            int r1 = mid1 == n ? INT_MAX : nums1[mid1];
-            int r2 = mid2 == m ? INT_MAX : nums2[mid2];
+        if(n>m)
+            return findMedianSortedArrays(nums2,nums1);
+        int l = 0, r = n;
+        while(l<=r) {
+            int mid1 = (l+r)/2;
+            int mid2 = (n+m+1)/2 - mid1;
+            int l1 = mid1==0?INT_MIN:nums1[mid1-1];
+            int l2 = mid2==0?INT_MIN:nums2[mid2-1];
+            int r1 = mid1==n?INT_MAX:nums1[mid1];
+            int r2 = mid2==m?INT_MAX:nums2[mid2];
             if (l1 <= r2 && l2 <= r1)
                 return (n + m) % 2 == 0
                         ? (max(l1, l2) + min(r1, r2)) * 0.5
@@ -29,6 +22,6 @@ public:
             else
                 l = mid1 + 1;
         }
-        return 0;
+        return -1;
     }
 };
