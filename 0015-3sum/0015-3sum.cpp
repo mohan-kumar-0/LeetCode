5b821ca5e36dd4a63884bfa3ccca6395e52de1a5
Mohan Kumar <32093111+mohan-kumar-0@users.noreply.github.com>
2024-10-04 22:42:39 +0530

    Time: 2251 ms (5.00%), Space: 504.8 MB (5.00%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,23 +5,21 @@ public:
         set<vector<int>> st;
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        int target;
-        for(int i=0;i<n;++i){
-            target = -nums[i];
-            for(int j=i+1,k=n-1;j<n && k>j;){
-                if(i!=j && i!=k){
-                    if(nums[j]+nums[k]==target){
-                        st.insert({nums[i],nums[j],nums[k]});
-                        ++j;
-                    }
-                    else if(nums[j]+nums[k]>target)
-                        --k;
-                    else
-                        ++j;
+        // cout<<"New TC \n\n";
+        for(int i=0;i<n-2;++i){
+            int left = i+1;
+            int right = n-1;
+            int target = -nums[i];
+            while(left<right){
+                if( (nums[left]+nums[right]) == target) {
+                    st.insert({nums[i],nums[left],nums[right]});
+                    ++left;
                 }
+                else if( (nums[left]+nums[right]) < target)
+                    ++left;
+                else
+                    --right;
             }
-            while((1+i)<n && nums[i+1]==nums[i])
-                ++i;
         }
         for(auto x: st)
             ans.push_back(x);
